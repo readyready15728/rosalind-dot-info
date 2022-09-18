899bc4234c89b00373ff8c0ae184a3b7c074b117
Lynn Bradshaw <readyready15728@gmail.com>
2022-09-18 11:58:37 -0400

    Adding ini3.py and data

---
+++ python-village/ini3.py
@@ -0,0 +1,7 @@
+from common import parse_integer_list
+
+with open('ini3-dataset.txt') as f:
+    mess = f.readline().strip()
+    a, b, c, d = parse_integer_list(f.readline())
+
+    print(' '.join([mess[a:b + 1], mess[c:d + 1]]))
