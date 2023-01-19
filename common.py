70f58e8b26b5e0470b607b192b67384309b1ba68
Lynn Bradshaw <readyready15728@gmail.com>
2023-01-19 16:44:46 +0000

    Adding FASTA parsing to common.py

--- common.py
+++ common.py
@@ -3,3 +3,16 @@ def parse_integer_list(line):
 
 def parse_word_list(line):
     return line.strip().split(' ')
+
+def parse_fasta_lines(lines):
+    fasta_dict = {}
+    current_label = None
+
+    for line in lines:
+        if line[0] == '>':
+            current_label = line.strip()[1:]
+            fasta_dict[current_label] = ''
+        else:
+            fasta_dict[current_label] += line.strip()
+
+    return fasta_dict
